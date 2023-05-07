23e97201999670b3e764e1004a7a3f55c4a4ffe5
JH <jinhyuk@unist.ac.kr>
2023-05-07 12:25:51 +0900

    modulize

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name="sghr",
+    version="0.0.0",
+    description="sghr",
+    long_description_content_type="text/markdown",
+    url="https://github.com/jk4011/sghr",
+    author="Jinhyeok Kim",
+    author_email="jinhyuk@unist.ac.kr",
+    install_requires=[],
+    packages=["TransSync"],
+    python_requires=">=3.7, <4",
+    project_urls={
+        "Source": "https://github.com/jk4011/sghr",
+    },
+)
